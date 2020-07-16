c7b006dfa13ea70563bd1ff7ff3a8b9cc89d93cf
Kshitij Sodani <52660406+Kshitij-789@users.noreply.github.com>
2020-07-16 17:57:45 +0530

    Add files via upload

--- Olympiad/MCO 16-acorn.cpp
+++ Olympiad/MCO 16-acorn.cpp
@@ -1,9 +1,7 @@
 /*
-The solution I got was same as https://github.com/luciocf/Problems/blob/master/MCO/MCO%202016/acorn.cpp
-
+The solution I got is similar to  https://github.com/luciocf/Problems/blob/master/MCO/MCO%202016/acorn.cpp
+just instead of the binary search I am doing math using inequalities.
 */
-
-
 #include <bits/stdc++.h>
 using namespace std;
 typedef long long llo;
@@ -14,26 +12,6 @@ typedef long long llo;
 
 llo n;
 vector<pair<llo,llo>> aa;
-vector<pair<llo,llo>> it;
-llo check(llo cc,llo ind){
-	pair<llo,llo> i=it[ind];
-	llo ind2=-1;
-	for(auto j:it){
-		ind2++;
-		if(ind2==ind){
-			continue;
-		}
-		llo ac=max((llo)0,(cc-j.a)*j.b);
-		llo bc=max((llo)0,(cc-i.a)*i.b);
-		if(ac>bc){
-			return 0;
-		}
-		if(ac==bc and ind2>ind){
-			return 0;
-		}
-	}
-	return 1;
-}
 int main(){
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -48,7 +26,7 @@ int main(){
 		aa[i].b=-aa[i].b;
 	}
 	llo ma=0;
-	
+	vector<pair<llo,llo>> it;
 	llo ans=0;
 	for(llo i=0;i<n;i++){
 		if(aa[i].b<=ma){
@@ -60,38 +38,14 @@ int main(){
 	//	cout<<aa[i].a<<",,"<<aa[i].b<<endl;
 	}
 	llo ind=-1;
-	llo low=1;
-	llo high=1e6+1;
 	for(auto i:it){
 		ind++;
-		low=max(low,i.a+1);
-		if(!check(low,ind)){
-			ans+=1;
-			continue;
-		}
-		
-		high=1e6+1;
-		while(low<high-1){
-			llo mid=(low+high)/2;
-			if(check(mid,ind)){
-				low=mid;
-			}
-			else{
-				high=mid;
-			}
-		}
-		llo anss=low;
-		if(check(high,ind)){
-			anss=max(anss,high);
-		}
-		low=anss+1;
-		
-		//llo ind2=-1;
-	/*	pair<llo,llo> mi={i.a+1,1};//time>=mi
-		pair<llo,llo> ma={1e6+1,1};//time<ma*/
+		llo ind2=-1;
+		pair<llo,llo> mi={i.a+1,1};//time>=mi
+		pair<llo,llo> ma={1e9+1,1};//time<ma
 		/*pair<llo,llo> mi={-1,1};
 		pair<llo,llo> ma={1e9+1,1};*/
-		/*for(auto j:it){
+		for(auto j:it){
 			ind2++;
 			if(ind2==ind){
 				continue;
@@ -111,28 +65,32 @@ int main(){
 					ma=kk;
 				}
 			}
-		}*/
+		}
 	//	cout<<i.a<<":"<<i.b<<endl;
 	//	cout<<mi.a<<":"<<mi.b<<"::"<<ma.a<<":"<<ma.b<<endl;
-		/*if(ind!=(llo)(it.size())-1){
+		/*if(ind!=(int)(it.size())-1){
 			if((i.a+1)*mi.b>mi.a){
 				mi={i.a+1,1};
 			}
 		}*/
-	/*	if(ma.a*mi.b>mi.a*ma.b){
+
+		if(ma.a*mi.b<=mi.a*ma.b){
+			ans+=1;
 		}
 		else{
-			if(ind==it.size()-1){
-				if(it[0].a>=1){
-					continue;
-				}
+			int xx=mi.a/mi.b;
+			int yy=mi.a/mi.b+1;
+			if(xx*mi.b>=mi.a and xx*ma.b<ma.a){
 			}
-			ans+=1;
-		}*/
+			else if(yy*mi.b>=mi.a and yy*ma.b<ma.a){
+			}
+			else{
+				ans+=1;
+			}
+		}
 		//break;
 	}
 	cout<<ans<<endl;
 
 	return 0;
 }
-
